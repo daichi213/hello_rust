eb8c5c61618c02bd780b0d970fa23d601471bd06
daichi213 <bestabokadon910@gmail.com>
2024-12-14 12:37:20 +0000

    [First Commit]Rustツアーの学習src追加

---
+++ rust_tour/3_basic_data_structures/1_memory.rs
@@ -0,0 +1,12 @@
+struct SeaCreature {
+    // Stringは構造体
+    animal_type: String,
+    name: String,
+    arms: i32,
+    legs: i32,
+    weapon: String,
+}
+
+fn main() {
+    let ferris = SeaCreature
+}
